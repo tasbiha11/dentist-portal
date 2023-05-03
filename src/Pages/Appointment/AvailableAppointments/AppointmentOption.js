2b973666bd2b0bbf919340eb8c6fd9816eacde08
tasbiha11 <tasbihahussain@yahoo.com>
2023-05-03 19:53:01 +0600

    appointment modal data

--- src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -11,6 +11,7 @@ const AppointmentOption = ({ appoitmentOption, setTreatment }) => {
                 <p>{slots.length}{slots.length > 1 ? ' spaces' : ' space'} available</p>
                 <div className="card-actions justify-center">
                     <label
+                        disabled={slots.length === 0}
                         htmlFor="booking-modal"
                         className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white"
                         onClick={() => setTreatment(appoitmentOption)}
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -1,9 +1,34 @@
 import { format } from 'date-fns';
 import React from 'react';
 
-const BookingModal = ({ treatment, selectedDate }) => {
-    const { name, slots } = treatment; //treatment is appointment options just different name
+const BookingModal = ({ treatment, setTreatment, selectedDate }) => {
+    //treatment is just another name of appointmentOptions name, slots, _id
+    const { name, slots } = treatment;
     const date = format(selectedDate, 'PP');
+
+    const handleBooking = event => {
+        event.preventDefault();
+        const form = event.target;
+        const slot = form.slot.value;
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+
+        const booking = {
+            appointmentDate: date,
+            treatment: name,
+            patient: name,
+            slot,
+            email,
+            phone
+        }
+        // TODO: send data to the server
+        //once data is saved then close the modal
+        //and display success toast
+        console.log(booking);
+        setTreatment(null);
+    }
+
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -11,16 +36,19 @@ const BookingModal = ({ treatment, selectedDate }) => {
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{name}</h3>
-                    <form className='grid grid-cols-1 gap-3 mt-10'>
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
                         <input type="text" disabled value={date} className="input input-bordered w-full" />
-                        <select className="select select-bordered w-full">
+                        <select name="slot" className="select select-bordered w-full">
                             {
-                                slots.map(slot => <option value={slot}>{slot}</option>)
+                                slots.map((slot, i) => <option
+                                    value={slot}
+                                    key={i}
+                                >{slot}</option>)
                             }
                         </select>
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
-                        <input type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <input name="name" type="text" placeholder="Your Name" className="input input-bordered w-full" />
+                        <input name="email" type="email" placeholder="Email" className="input input-bordered w-full" />
+                        <input name="phone" type="text" placeholder="Phone" className="input input-bordered w-full" />
                         <br />
                         <input className='btn btn-accent w-full' type="submit" value="Submit" />
                     </form>
